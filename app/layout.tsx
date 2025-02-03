35a3ee6987969db826b8603bb4997edfda07514e
Ahmed El Hadad <ahmedha95@outlook.com>
2025-02-03 20:43:16 +0200

    minor fixes to UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         <Header />
-        <div className="fixed -bottom-1 left-0 z-50 w-full bg-white py-2 shadow-md dark:bg-zinc-900 lg:hidden">
+        <div className="fixed -bottom-1 left-0 z-50 w-full rounded-t-[1rem] bg-white py-2 shadow-md dark:bg-zinc-900 lg:hidden">
           <MobileNavbar />
         </div>
         <main className="relative dark:bg-zinc-900 dark:bg-gradient-to-r dark:from-zinc-900 dark:to-zinc-800">
