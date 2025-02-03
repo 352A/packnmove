1494e0550519f118d82181e395912012179573af
Ahmed El Hadad <ahmedha95@outlook.com>
2025-02-03 17:49:04 +0200

    minor fixes to UI

--- app/_components/home/Hero9.tsx
+++ app/_components/home/Hero9.tsx
@@ -3,12 +3,12 @@ import { Button } from "../ui/button";
 export default function Hero9() {
   return (
     <section className="relative bg-zinc-50 dark:bg-transparent lg:py-12 lg:pt-16 xl:px-12">
-      <div className="right-0 flex w-full justify-end overflow-hidden lg:absolute xl:px-12 xl:pl-52">
+      <div className="sticky right-0 top-0 flex w-full justify-end overflow-hidden lg:absolute xl:px-12 xl:pl-52">
         <video
           autoPlay
           muted
           loop
-          className="h-screen w-full rounded-br-none object-cover lg:h-[29rem] lg:w-[35rem] lg:rounded-bl-[15rem] lg:rounded-tl-[15rem] lg:rounded-tr-[20rem] xl:w-1/2"
+          className="h-[25rem] w-full rounded-br-none object-cover lg:h-[29rem] lg:w-[35rem] lg:rounded-bl-[15rem] lg:rounded-tl-[15rem] lg:rounded-tr-[20rem] xl:w-1/2"
         >
           <source
             src="https://res.cloudinary.com/dihbojmlf/video/upload/v1737661122/packnmove720p_vzpcjm.mp4"
@@ -17,7 +17,7 @@ export default function Hero9() {
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="flex rounded-b-[5rem] bg-zinc-900 object-cover py-12 align-middle dark:bg-zinc-900 dark:bg-gradient-to-r dark:from-zinc-950 dark:to-zinc-800 max-lg:z-20 max-lg:px-14 lg:h-[29rem] lg:w-3/4 lg:rounded-bl-none lg:rounded-br-[15rem] lg:rounded-tl-[15rem] lg:rounded-tr-[15rem] lg:ps-24 lg:pt-16 xl:rounded-tl-[20rem] xl:ps-52">
+      <div className="max-lg: flex rounded-b-[5rem] bg-zinc-900 object-cover py-12 align-middle dark:bg-zinc-900 dark:bg-gradient-to-r dark:from-zinc-950 dark:to-zinc-800 max-lg:sticky max-lg:z-20 max-lg:px-14 lg:h-[29rem] lg:w-3/4 lg:rounded-bl-none lg:rounded-br-[15rem] lg:rounded-tl-[15rem] lg:rounded-tr-[15rem] lg:ps-24 lg:pt-16 xl:rounded-tl-[20rem] xl:ps-52">
         <div className="grid items-center gap-12 lg:grid-cols-2">
           <div className="flex flex-col gap-9 max-lg:text-center lg:gap-8">
             <h1 className="text-3xl font-bold text-zinc-100 dark:text-zinc-100 lg:text-4xl">
@@ -34,6 +34,7 @@ export default function Hero9() {
           </div>
         </div>
       </div>
+      <div className="absolute bottom-0 h-20 w-full dark:bg-gradient-to-r dark:from-zinc-900 dark:to-zinc-800 lg:hidden"></div>
     </section>
   );
 }
--- app/_components/home/Process.tsx
+++ app/_components/home/Process.tsx
@@ -40,7 +40,7 @@ export const projects = [
 
 export default function Process() {
   return (
-    <section className="relative w-full bg-gradient-to-r dark:bg-zinc-900 dark:from-zinc-900 dark:to-zinc-800 max-lg:px-6 max-lg:py-12">
+    <section className="relative w-full bg-gradient-to-r dark:bg-zinc-900 dark:from-zinc-900 dark:to-zinc-800 max-lg:px-6 max-lg:py-20">
       <div className="w-full bg-zinc-50 bg-gradient-to-r dark:bg-zinc-900 dark:from-zinc-900 dark:to-zinc-800 lg:rounded-b-[8rem] lg:border-b-[0.3rem] lg:border-zinc-800 lg:py-8 lg:dark:border-white">
         <h2 className="text-center text-2xl font-bold leading-relaxed text-zinc-800 dark:text-zinc-100 lg:text-4xl">
           PROFESSIONAL & QUICK PROCESS
