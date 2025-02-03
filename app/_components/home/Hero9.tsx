9d043fc89a5128db9569931057d7317e3b35e8fc
Ahmed El Hadad <ahmedha95@outlook.com>
2025-02-03 21:53:04 +0200

    minor fixes to UI

--- app/_components/home/Hero9.tsx
+++ app/_components/home/Hero9.tsx
@@ -17,7 +17,7 @@ export default function Hero9() {
           Your browser does not support the video tag.
         </video>
       </div>
-      <div className="flex rounded-b-[4rem] bg-zinc-900 object-cover py-12 align-middle dark:bg-zinc-900 dark:bg-gradient-to-r dark:from-zinc-950 dark:to-zinc-800 max-lg:sticky max-lg:z-20 max-lg:px-2 lg:h-[29rem] lg:w-3/4 lg:rounded-bl-none lg:rounded-br-[15rem] lg:rounded-tl-[15rem] lg:rounded-tr-[15rem] lg:ps-24 lg:pt-16 xl:rounded-tl-[20rem] xl:ps-52">
+      <div className="flex rounded-b-[4rem] bg-zinc-900 object-cover py-12 align-middle dark:bg-zinc-900 dark:bg-gradient-to-r dark:from-zinc-950 dark:to-zinc-800 max-lg:sticky max-lg:z-20 max-lg:px-6 lg:h-[29rem] lg:w-3/4 lg:rounded-bl-none lg:rounded-br-[15rem] lg:rounded-tl-[15rem] lg:rounded-tr-[15rem] lg:ps-24 lg:pt-16 xl:rounded-tl-[20rem] xl:ps-52">
         <div className="grid items-center gap-12 lg:grid-cols-2">
           <div className="flex flex-col gap-9 max-lg:text-center lg:gap-8">
             <h1 className="text-3xl font-bold text-zinc-100 dark:text-zinc-100 lg:text-4xl">
